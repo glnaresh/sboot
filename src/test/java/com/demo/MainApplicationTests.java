be1ab6cca1226b5adf77bd066a842fc3b0e405f6
nareshlakshmi <naresh.lakshmi@accenture.com>
2019-03-04 16:49:58 +0100

    correcting the test class name

--- src/test/java/com/demo/MainApplicationTests.java
+++ src/test/java/com/demo/MainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.demo;
+package com.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class MainApplicationTests {
 
 	@Test
 	public void contextLoads() {
