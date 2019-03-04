14b71b7ea9f9560b3acb7396839bcb681042eb72
nareshlakshmi <naresh.lakshmi@accenture.com>
2019-03-04 15:57:08 +0100

    ensuring my main class is picked up by spring boot

--- src/main/java/com/demo/MainApplication.java
+++ src/main/java/com/demo/MainApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
 		SpringApplication.run(MainApplication.class, args);
 	}
 
